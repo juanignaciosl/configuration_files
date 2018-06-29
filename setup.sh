30b7f3d8a3272bbb4a9df94ddf476e0a5ccbe747
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-06-29 14:31:29 +0200

    .carto-dev-cfg.yml

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 CONFIGURATION_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-HomeDotfiles=('.bashrc' '.gitconfig' '.gitignore' '.git-prompt.sh' '.inputrc' '.vimrc' '.zshrc')
+HomeDotfiles=('.bashrc' 'CARTO/.carto-dev-cfg.yml' '.gitconfig' '.gitignore' '.git-prompt.sh' '.inputrc' '.vimrc' '.zshrc')
 
 for homeDotFile in "${HomeDotfiles[@]}"; do
   DOT_FILE_PATH=$CONFIGURATION_FILES_DIR/$homeDotFile
