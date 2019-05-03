5639e1b5cbd0680e8d59764d2ab5f3913f99cd4d
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2019-05-03 09:11:09 +0200

    CARTO-specific setup

---
+++ CARTO/setup.sh
@@ -0,0 +1,14 @@
+CONFIGURATION_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+HomeDotfiles=('.cic.cfg')
+
+for homeDotFile in "${HomeDotfiles[@]}"; do
+  HOME_FILE_PATH=$HOME/$homeDotFile
+  if [ -f $HOME_FILE_PATH ] && [ ! -h $HOME_FILE_PATH ]; then
+    echo "$HOME_FILE_PATH exists and it's not a symbolic link"
+    exit 1
+  fi
+
+  DOT_FILE_PATH=$CONFIGURATION_FILES_DIR/$homeDotFile
+  ln -sfT $DOT_FILE_PATH $HOME_FILE_PATH
+done
