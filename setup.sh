3e0354e522dba1622a18ba4e5feceda4b79e6afe
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2019-05-20 09:08:47 +0200

    Fix ssh files permissions

--- setup.sh
+++ setup.sh
@@ -25,4 +25,7 @@ for homeDotFile in "${HomeDotfiles[@]}"; do
   ln -sfT $DOT_FILE_PATH $HOME_FILE_PATH
 done
 
+# .ssh files require 700 permissions
+chmod 700 /home/juanignaciosl/.ssh/config
+
 sudo apt install -y exfat-fuse exfat-utils vim git libcanberra-gtk-module gnome-tweak-tool clamav lxc lxc-templates cgroup-lite redir bridge-utils npm ngrep terminator virtualenv htop telegram-desktop gparted docker docker-compose postgresql-client powertop darktable python-dev flameshot xclip
