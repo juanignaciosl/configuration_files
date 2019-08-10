b3f741f57e11f5be1aeb49caa25ec23cc489748b
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2019-08-10 10:45:37 +0200

    XPS 7590 Arch Linux revamp

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Note that installing `sudo` is first needed
+
+# Yay
+sudo pacman -S git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Base packages
+
+sudo pacman -S vim git gnome networkmanager firefox-developer-edition base-devel gnome-tweaks extra/ttf-dejavu ruby htop terminator x264
+
+yay -S dropbox bash-git-prompt intellij-toolbox spotify telegram-desktop-bin vlc
