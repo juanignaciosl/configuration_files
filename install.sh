193734d795597d42250e44b20537a0441f6c773b
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2020-09-27 10:15:39 +0200

    Updated package list

--- install.sh
+++ install.sh
@@ -8,8 +8,10 @@ makepkg -si
 
 # Base packages
 
-sudo pacman -S vim git gnome networkmanager firefox-developer-edition base-devel gnome-tweaks extra/ttf-dejavu ruby htop terminator x264 docker bluez bluez-utils 
+sudo pacman -S vim git gnome networkmanager firefox-developer-edition base-devel gnome-tweaks extra/ttf-dejavu ruby htop terminator x264 docker bluez bluez-utils inetutils
 
-yay -S dropbox bash-git-prompt intellij-toolbox spotify telegram-desktop-bin vlc bfg gnome-shell-extension-appindicator debtap ttf-twemoji libinput-gestures
+yay -S nvm dropbox bash-git-prompt jetbrains-toolbox spotify telegram-desktop-bin vlc bfg gnome-shell-extension-appindicator debtap ttf-twemoji libinput-gestures rescuetime2 gnome-shell-extension-tray-icons
 
 # libinput-gestures requires running `libinput-gestures-setup start`
+
+sudo systemctl enable NetworkManager
